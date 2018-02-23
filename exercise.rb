51c5093ee47406e23fd0dbcee7130848e596cbcc
dmitrysbn <dmitry.sbn@gmail.com>
2018-02-23 16:11:24 -0500

    Add seating options

--- exercise.rb
+++ exercise.rb
@@ -1,3 +1,5 @@
+require 'ap'
+
 rows = [
   [nil, "Pumpkin", nil, nil],
   ["Socks", nil, "Mimi", nil],
@@ -7,8 +9,15 @@ rows = [
 
 rows.each_with_index do |row, row_number|
   row.each_with_index do |seat, seat_number|
-    if seat == nil
-      puts "Row #{row_number + 1} seat #{seat_number + 1} is free."
+    if seat.nil?
+      puts "Row #{row_number + 1} seat #{seat_number + 1} is free. Do you want to sit there? (y/n)"
+      answer = gets.chomp
+      if answer.downcase == "y"
+        puts "What is your name?"
+        name = gets.chomp
+        rows[row_number][seat_number] = name
+        return ap rows
+      end
     end
   end
 end
